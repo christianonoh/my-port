7dc786e42391dd9c23c978e702b07b61139765a6
christianonoh <onohchibyk@gmail.com>
2023-10-07 11:37:10 +0100

    Modify Sanity client

--- sanity/sanity.client.ts
+++ sanity/sanity.client.ts
@@ -1,11 +1,11 @@
-import { createClient } from "@sanity/client"
+import { createClient, type ClientConfig } from "@sanity/client"
 
-const clientConfig = {
+const config: ClientConfig = {
   "projectId": "n5aosvhy",
   "dataset": "production",
   "apiVersion": "2023-10-06"
 }
 
-const sanityClient = createClient(clientConfig);
+const sanityClient = createClient(config);
 
 export default sanityClient;
