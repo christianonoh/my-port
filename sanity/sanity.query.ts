780067f452400b83870e0646456fd4f1ebe47fb6
christianonoh <onohchibyk@gmail.com>
2023-10-07 11:37:22 +0100

    Modify Sanity query

--- sanity/sanity.query.ts
+++ sanity/sanity.query.ts
@@ -1,2 +1,23 @@
+import { groq } from 'next-sanity';
 import sanityClient from "./sanity.client";
 
+
+const getProfile = async () => {
+  return sanityClient.fetch(
+    groq`*[_type == "profile"][0]{
+      _id,
+      fullName,
+      headline,
+      profileImage {alt, "image": asset->url},
+      shortBio,
+      location,
+      fullBio,
+      email,
+      "resumeURL": resumeURL.asset->url,
+      socialLinks,
+      skills
+      }
+    }
+  `);
+}
+export default getProfile;
