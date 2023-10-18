2e3b199ad5a843a0b7be55ae61bdc4c10ff57f01
christianonoh <onohchibyk@gmail.com>
2023-10-18 16:57:06 +0100

    Create footer component

---
+++ app/(site)/components/global/Footer.tsx
@@ -0,0 +1,23 @@
+const Footer = () => {
+  return (
+    <footer className="border-t border-zinc-800 mt-44">
+      <div className="max-w-7xl mx-auto flex lg:flex-row flex-col items-center lg:justify-between justify-center gap-y-4 md:px-16 px-6 py-16 text-zinc-400">
+        <small className=" duration-200 font-mono">
+          All rights reserved &copy; {new Date().getFullYear()}
+        </small>
+
+        <small className="hover:text-white duration-200">
+          <a
+            href="https://github.com/christianonoh"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Developed by <span className="text-green-400">Christian Onoh</span>
+          </a>
+        </small>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
