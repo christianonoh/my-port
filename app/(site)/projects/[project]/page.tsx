bfb05f8b635a29bca63ff5c93122351b53bb0df4
christianonoh <onohchibyk@gmail.com>
2023-11-10 18:29:24 +0100

    Refactor project page layout

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -30,10 +30,10 @@ const Project = async ({ params }: Props) => {
   const slug = params.project;
   const project = await getProject(slug);
   return (
-    <main className="max-w-6xl mx-auto lg:px-16 px-8">
+    <main className="max-w-6xl px-8 mx-auto lg:px-16">
       <div className="max-w-3xl mx-auto">
         <div className="flex items-start justify-between mb-4">
-          <h1 className="font-bold lg:text-5xl text-3xl lg:leading-tight mb-4">
+          <h1 className="mb-4 text-3xl font-bold lg:text-5xl lg:leading-tight">
             {project.title}
           </h1>
 
@@ -46,13 +46,13 @@ const Project = async ({ params }: Props) => {
           </a>
         </div>
         <Image
-          className="rounded-xl border border-zinc-800"
+          className="border rounded-xl border-zinc-800"
           width={900}
           height={460}
           src={project.coverImage?.image}
           alt={project.coverImage?.alt || project.title}
         />
-        <div className="flex flex-col gap-y-6 mt-8 leading-7 text-zinc-400">
+        <div className="flex flex-col mt-8 leading-7 gap-y-6 text-zinc-400">
           <PortableText value={project.description} />
         </div>
       </div>
