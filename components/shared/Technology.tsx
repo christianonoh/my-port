19d040fbcace4f267b2fab710b57b037f39d3995
Christian Onoh <chibyk5000@gmail.com>
2023-12-14 22:36:22 +0100

    Handle description overflow and add fade in effect

--- components/shared/Technology.tsx
+++ components/shared/Technology.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Link from "next/link";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 const Technology = ({
@@ -9,44 +9,63 @@ const Technology = ({
   description,
   href = "#",
   enableHover = true,
+  slug,
 }: {
   title: string;
   className?: string;
   href?: string;
   description: string;
   enableHover?: boolean;
+  slug?: string;
 }) => {
   const [showDescription, setShowDescription] = useState(false);
+  const [techSlug, setTechSlug] = useState("");
 
-  const handleMouseEnter = () => {
+  const handleMouseEnter = (slug: string) => {
     if (enableHover) {
       setShowDescription(true);
+      setTechSlug(slug);
     }
   };
 
   const handleMouseLeave = () => {
     if (enableHover) {
       setShowDescription(false);
+      setTechSlug("");
     }
   };
 
   return (
-    <Link
+    <div
       key={title}
-      href={href}
-      className={`relative bg-[#1d1d20] border border-transparent hover:border-zinc-700 rounded-md px-2 py-1 ${
-        enableHover ? "cursor-pointer" : ""
+      className={`relative bg-[#1d1d20] border border-transparent technology hover:border-zinc-700 rounded-md px-2 py-1 ${slug}  ${
+        enableHover ? " cursor-pointer" : ""
       }`}
-      onMouseEnter={handleMouseEnter}
+      onMouseEnter={() => handleMouseEnter(slug ?? "")}
       onMouseLeave={handleMouseLeave}
     >
       {title}
       {enableHover && showDescription && (
-        <div className="absolute bg-accent text-center text-xs text-white p-2 w-full min-w-[384px] z-10 top-8 right-1/2 translate-x-1/2 rounded-md mt-2 transition-all ease-in-out duration-300">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+          className={`absolute bg-accent text-center text-xs text-white p-2 w-48 z-10 top-8 rounded-md mt-2 transition-all ease-in-out duration-1000 ${
+            // Check if the hovered item exists and if there's more space on the right
+            document.querySelector(`.${techSlug}`) &&
+            window.innerWidth -
+              (document.querySelector(`.${techSlug}`)!.getBoundingClientRect()
+                .left +
+                192) <
+              50
+              ? "right-0"
+              : ""
+          }`}
+        >
           {description}
-        </div>
+        </motion.div>
       )}
-    </Link>
+    </div>
   );
 };
 
