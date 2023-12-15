c6ae762dbae37baf02fb7395f7d7e4078c69104e
Christian Onoh <chibyk5000@gmail.com>
2023-12-15 21:50:25 +0100

    Add fields for project summary, technology stack, features, problem statement, and milestone

--- sanity/schemas/project-schema.ts
+++ sanity/schemas/project-schema.ts
@@ -20,6 +20,14 @@ const project = {
       validation: (rule) => rule.max(60).required(),
     }),
     defineField({
+      name: "summary",
+      title: "Summary",
+      type: "array",
+      description: "Write a summary of the project",
+      of: [{ type: "block" }],
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
       name: "slug",
       title: "Slug",
       type: "slug",
@@ -29,10 +37,30 @@ const project = {
       validation: (rule) => rule.required(),
     }),
     defineField({
-      name: "technologies",
-      title: "Technologies",
+      name: "stack",
+      title: "Technology Stack",
       type: "array",
-      of: [{ type: "reference", to: { type: "technology" } }],
+      of: [
+        {
+          type: "object",
+          fields: [
+            {
+              name: "key",
+              title: "Key",
+              description: "What technology was used?",
+              type: "reference",
+              to: [{ type: "technology" }],
+            },
+            {
+              name: "value",
+              title: "Value",
+              type: "string",
+              description: "What was it was used for?",
+            },
+          ],
+        },
+      ],
+      validation: (rule) => rule.required(),
     }),
     defineField({
       name: "logo",
@@ -56,14 +84,19 @@ const project = {
       validation: (rule) => rule.required(),
     }),
     defineField({
-      name: "description",
-      title: "Description",
+      name: "features",
+      title: "Feautures",
       type: "array",
       description: "Write a full description about this project",
       of: [{ type: "block" }],
-      validation: (rule) => rule.required(),
     }),
-
+    defineField({
+      name: "problemStatement",
+      title: "Problem Statement",
+      type: "array",
+      description: "Define the challenges addressed by this project.",
+      of: [{ type: "block" }],
+    }),
     {
       name: "projectUrl",
       title: "Project URL",
@@ -73,7 +106,15 @@ const project = {
       name: "githubUrl",
       title: "Github URL",
       type: "url",
+      initialValue: "https://github.com/",
     },
+    defineField({
+      name: "milestone",
+      title: "Milestone",
+      type: "array",
+      description: "What notable achievements has this project accomplished?",
+      of: [{ type: "block" }],
+    }),
   ],
 };
 
