296880157c28079ac30bdcc415c73fe83200a90d
Christian Onoh <chibyk5000@gmail.com>
2023-12-15 18:08:59 +0100

    Add validation for description field in technology schema

--- sanity/schemas/technology.ts
+++ sanity/schemas/technology.ts
@@ -25,6 +25,8 @@ export default defineType({
       name: "description",
       title: "Description",
       type: "text",
+      validation: (Rule) =>
+        Rule.max(200).warning("Shorter descriptions are encouraged."),
     }),
     defineField({
       name: "logo",
