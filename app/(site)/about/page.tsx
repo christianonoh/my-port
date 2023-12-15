c921cd4420050551517b99a17771423641c8553d
Christian Onoh <chibyk5000@gmail.com>
2023-12-15 17:42:30 +0100

    Refactor about page components and import Expertise component

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -1,26 +1,27 @@
-import { getProfile, getJobs, getTechnologies } from "@/sanity/sanity.fetch";
+import { getProfile, getJobs, getEducations } from "@/sanity/sanity.fetch";
 import { PortableText } from "@portabletext/react";
 import Image from "next/image";
 import { BiEnvelope, BiDownload, BiLinkExternal } from "react-icons/bi";
-import Job from "../../../components/Job";
 import TransitionEffect from "@/components/TransitionEffect";
 import AnimatedText from "@/components/AnimatedText";
-import Technology from "@/components/shared/Technology";
+import Experience from "@/components/about/Experience";
+import Expertise from "@/components/about/Expertise";
+import Education from "@/components/about/Education";
 
 const About = async () => {
   const profile = await getProfile();
   const jobs = await getJobs();
-  const technologies = await getTechnologies();
+  const schools = await getEducations();
+
   return (
     <>
       <TransitionEffect />
-      <main className="max-w-7xl mx-auto px-6 md:px-12 lg:px-16 relative">
+      <main className="max-w-7xl mx-auto px-6 md:px-12 lg:px-16 relative py-12 lg:py-32">
         {profile &&
           profile.map((data) => (
             <div key={data._id}>
-              <section className="grid lg:grid-cols-2 grid-cols-1 gap-x-6 justify-items-center mt-12 lg:mt-32">
+              <section className="grid lg:grid-cols-2 grid-cols-1 gap-x-6 justify-items-center ">
                 <div className="order-2 lg:order-none">
-                  {/* <h1 className=" lg:leading-tight basis-1/2 font-bold mb-8"></h1> */}
                   <AnimatedText
                     text={`Friends call me Burger 🍔 But I'm also ${data.fullName}`}
                     className="mb-8 lg:!text-6xl sm:!text-4xl md:!text-5xl  !text-3xl lg:leading-relaxed leading-relaxed text-center lg:text-left max-w-xl"
@@ -37,6 +38,8 @@ const About = async () => {
                         width={400}
                         height={600}
                         quality={100}
+                        placeholder="blur"
+                        blurDataURL={data.profileImage.image.metadata.lqip}
                         alt={data.profileImage.alt}
                         className="rounded-2xl mb-4 object-cover object-top border-2 transform scale-x-[-1] max-h-96 min-h-96 bg-top bg-[#1d1d20]"
                       />
@@ -64,7 +67,7 @@ const About = async () => {
                       <li>
                         <a
                           href={`mailto:${data.email}`}
-                          className="flex items-center gap-x-2 hover:text-purple-400 duration-300"
+                          className="flex items-center gap-x-2 hover:text-accent duration-300"
                         >
                           <BiEnvelope className="text-lg" />
                           {data.email}
@@ -74,34 +77,11 @@ const About = async () => {
                   </span>
                 </div>
               </section>
-              <section className="mt-24 max-w-[731.250px] mx-auto lg:mx-0 lg:max-w-2xl">
-                <h2 className="font-semibold text-4xl mb-4">Expertise</h2>
-                <p className="text-zinc-400 max-w-lg">
-                  I&apos;ve spent few years working on my skills. In no
-                  particular order, here are a few of them.
-                </p>
-                <ul className="flex flex-wrap items-center gap-3 mt-8">
-                  {technologies?.map((tech, id) => (
-                    <li key={id}>
-                      <Technology
-                        title={tech.title}
-                        description={tech.description}
-                      />
-                    </li>
-                  ))}
-                </ul>
-              </section>
             </div>
           ))}
-        <section className="mt-32 max-w-[731.250px] mx-auto lg:mx-0">
-          <div className="mb-16">
-            <h2 className="font-semibold text-4xl mb-4">Work Experience</h2>
-          </div>
-
-          <div className="flex flex-col gap-y-12">
-            {jobs && jobs.map((job) => <Job job={job} key={job._id} />)}
-          </div>
-        </section>
+        <Expertise />
+        <Experience jobs={jobs} />
+        <Education schools={schools} />
       </main>
     </>
   );
