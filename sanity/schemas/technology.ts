71348a467f0746f9f1de92e8d8f0db3ce629ec42
Christian Onoh <chibyk5000@gmail.com>
2023-12-15 17:48:43 +0100

    Add technology logo field to technology schema

--- sanity/schemas/technology.ts
+++ sanity/schemas/technology.ts
@@ -1,28 +1,44 @@
-import {defineField, defineType} from 'sanity'
+import { defineField, defineType } from "sanity";
+import { VscTools } from "react-icons/vsc";
 
 export default defineType({
-  name: 'technology',
-  title: 'Technology',
-  type: 'document',
+  name: "technology",
+  title: "Technology",
+  icon: VscTools,
+  type: "document",
   fields: [
     defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
+      name: "title",
+      title: "Title",
+      type: "string",
     }),
     defineField({
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
       options: {
-        source: 'title',
+        source: "title",
         maxLength: 30,
       },
     }),
     defineField({
-      name: 'description',
-      title: 'Description',
-      type: 'text',
+      name: "description",
+      title: "Description",
+      type: "text",
+    }),
+    defineField({
+      name: "logo",
+      title: "Technology Logo",
+      type: "image",
+      description: "Upload a logo for this technology",
+      options: { hotspot: true },
+      fields: [
+        {
+          name: "alt",
+          title: "Alt",
+          type: "string",
+        },
+      ],
     }),
   ],
-})
+});
