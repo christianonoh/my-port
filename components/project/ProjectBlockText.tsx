339e576e9ce570cbe5310ecedb36131c718a9ec5
Christian Onoh <chibyk5000@gmail.com>
2023-12-16 16:42:05 +0100

    Refactor ProjectBlockText component styles for responsiveness and dark mode

--- components/project/ProjectBlockText.tsx
+++ components/project/ProjectBlockText.tsx
@@ -14,13 +14,13 @@ const ProjectBlockText = ({
     <div className="flex flex-col mt-8 mb-4">
       <a href={`#${slug}`}>
         <h3
-          className="capitalize headlink font-semibold text-2xl mt-12 "
+          className="capitalize headlink font-semibold text-2xl mt-12"
           id={slug}
         >
           {title}
         </h3>
       </a>
-      <div className="flex flex-col mt-8 prose prose-lg leading-7 gap-y-6 text-gray">
+      <div className="flex flex-col mt-8 prose leading-7 gap-y-6 prose-sm  sm:prose-lg dark:text-gray text-gray-dark">
         <PortableText value={content} />
       </div>
     </div>
