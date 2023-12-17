512be1e3a4a956ae2e844e9d8c0dbbb6ad9c0895
Christian Onoh <chibyk5000@gmail.com>
2023-12-17 12:52:53 +0100

    Add siteMetaData module

---
+++ utils/siteMetaData.ts
@@ -0,0 +1,22 @@
+const siteMetadata = {
+  title: "Christian Onoh",
+  author: "Christian Onoh",
+  position: "Software Developer",
+  description:
+    "Christian Onoh is a software developer who is passionate about building solutions with a focus on high-performance, design, and user experience. ",
+  language: "en-us",
+  theme: "system", // system, dark, or light
+  keywords:
+    "Portfolio, Full-stack developer, React, Node.js, Ruby, Ruby on Rails, Next.js, Front-end developer, Back-end developer, Software engineer, TypeScript development, Web developer, Responsive web design, UI/UX design, JavaScript development, HTML5, CSS3, Modern web development, MVC architecture, RESTful APIs, Single-page applications (SPAs), Mobile-friendly development, Code optimization, Cross-browser compatibility, Server-side rendering (SSR)",
+  siteUrl: "https://christianonoh.vercel.app", // Update to your actual domain
+  siteLogo: "/logo.svg",
+  socialBanner:
+    "https://cdn.sanity.io/images/n5aosvhy/production/c1867d1d11f6ef978f6ad78af246956ada5d2786-1640x924.jpg", // add social banner in the public folder
+  email: "chibyk5000@gmail.com",
+  github: "https://github.com/christianonoh",
+  twitter: "https://twitter.com/onohchristian",
+  linkedin: "https://www.linkedin.com/in/christianonoh/",
+  locale: "en-US",
+};
+
+export default siteMetadata;
