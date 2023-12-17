820bc2126a330071a99992a01ac12e97e24a3562
Christian Onoh <chibyk5000@gmail.com>
2023-12-17 14:37:45 +0100

    Fix sanity image builder function

--- sanity/sanity.image.ts
+++ sanity/sanity.image.ts
@@ -8,10 +8,5 @@ const imageBuilder = createImageUrlBuilder({
 });
 
 export const urlForImage = (source: Image) => {
-  // Ensure that source image contains a valid reference
-  if (!source?.asset?._ref || !source?._type) {
-    return undefined;
-  }
-
   return imageBuilder?.image(source).auto("format").fit("max");
 };
